47f660bc345dc4cf1bcb5dd9ac25c04764184617
Ramya.AK <ramya.ak.developer>
2024-03-26 11:40:53 +0530

    updated env files

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
   <div>interview</div>
+
+  <div>
+    Package.json will have high priority than other Next will be the specific one i.e
+    env.local.development will be next priority than general one
+  </div>
 </template>
 
 <script setup lang="ts">
